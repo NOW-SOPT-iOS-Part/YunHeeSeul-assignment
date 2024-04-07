e4efddba2d2e5706a94f4fa67eb01258b352de03
seuriseuljjeok <heeseul09@naver.com>
2024-04-08 03:40:47 +0900

    [Setting] 프로젝트 기본 세팅 (#1)

---
+++ Tving_CloneProject/Tving_CloneProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tving_CloneProject
+//
+//  Created by 윤희슬 on 4/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
