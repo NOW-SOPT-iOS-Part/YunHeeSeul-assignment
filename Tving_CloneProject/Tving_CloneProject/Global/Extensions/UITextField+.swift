070376f948100be80c8be8a2d6bb256f267d2f57
seuriseuljjeok <heeseul09@naver.com>
2024-04-09 01:04:46 +0900

    [Add] extension 추가 (#1)

---
+++ Tving_CloneProject/Tving_CloneProject/Global/Extensions/UITextField+.swift
@@ -0,0 +1,44 @@
+//
+//  UITextField+.swift
+//  Tving_CloneProject
+//
+//  Created by 윤희슬 on 4/9/24.
+//
+
+import UIKit
+
+extension UITextField {
+    
+     func setLeftPadding(amount: CGFloat) {
+         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+         self.leftView = paddingView
+         self.leftViewMode = .always
+     }
+     func setRightPadding(amount: CGFloat) {
+         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+         self.rightView = paddingView
+         self.rightViewMode = .always
+     }
+    
+    //TextField 기본 속성 커스텀
+    func setTextField(forBackgroundColor: UIColor, forBorderColor: UIColor, forBorderWidth: CGFloat, forCornerRadius: CGFloat? = nil) {
+          self.clipsToBounds = true
+          self.layer.borderColor = forBorderColor.cgColor
+          self.layer.borderWidth = forBorderWidth
+          self.backgroundColor = forBackgroundColor
+          
+          if let cornerRadius = forCornerRadius {
+              self.layer.cornerRadius = cornerRadius
+          }  else {
+              self.layer.cornerRadius = 0
+          }
+    }
+    
+    
+    //TextField placeholder 커스텀
+    func setPlaceholder(placeholder: String, fontColor: UIColor?, font: UIFont) {
+        self.attributedPlaceholder = NSAttributedString(string: placeholder,
+                                                        attributes: [.foregroundColor: fontColor!,
+                                                                     .font: font])
+    }
+ }
---
+++ Tving_CloneProject/Tving_CloneProject/Global/Extensions/UIView+.swift
@@ -0,0 +1,24 @@
+//
+//  UIView+.swift
+//  Tving_CloneProject
+//
+//  Created by 윤희슬 on 4/9/24.
+//
+
+import UIKit
+
+extension UIView {
+    
+    // UIView 여러 개 인자로 받아서 한 번에 addSubview
+    func addSubviews(_ views: UIView...) {
+        views.forEach { self.addSubview($0) }
+    }
+    
+    //모서리둥글게
+    func roundCorners(cornerRadius: CGFloat, maskedCorners: CACornerMask) {
+        clipsToBounds = true
+        layer.cornerRadius = cornerRadius
+        layer.maskedCorners = CACornerMask(arrayLiteral: maskedCorners)
+    }
+
+}
