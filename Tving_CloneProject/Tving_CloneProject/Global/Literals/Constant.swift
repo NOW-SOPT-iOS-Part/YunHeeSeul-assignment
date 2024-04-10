f91406b1a161eedcae7411a01bca1df8842703c2
seuriseuljjeok <heeseul09@naver.com>
2024-04-11 01:54:56 +0900

    [Add] 기기대응 위한 Utils 추가 (#1)

---
+++ Tving_CloneProject/Tving_CloneProject/Global/Literals/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Tving_CloneProject
+//
+//  Created by 윤희슬 on 4/11/24.
+//
+
+import UIKit
+
+enum Constant {
+    struct Screen {
+        public static let width = UIScreen.main.bounds.width
+        public static let height = UIScreen.main.bounds.height
+    }
+}
