e8af0f4b2829e43df4eecf28314a1196bcf8a4db
seuriseuljjeok <heeseul09@naver.com>
2024-04-26 18:22:53 +0900

    [Feat] segement control 추가 (#3)

---
+++ Tving_CloneProject/Tving_CloneProject/Scene/Main/Views/HeaderCategoryView.swift
@@ -0,0 +1,20 @@
+//
+//  HeaderCategoryView.swift
+//  Tving_CloneProject
+//
+//  Created by 윤희슬 on 4/26/24.
+//
+
+import UIKit
+
+class HeaderCategoryView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
