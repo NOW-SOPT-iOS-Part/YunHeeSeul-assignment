eb9f53f2c50863824e4f74994e9aa9c16f46b68f
seuriseuljjeok <heeseul09@naver.com>
2024-05-06 23:17:29 +0900

    [Setting] 네트워크 통신 세팅 (#5)

---
+++ Tving_CloneProject/Tving_CloneProject/Network/Main/MainService.swift
@@ -0,0 +1,8 @@
+//
+//  MainService.swift
+//  Tving_CloneProject
+//
+//  Created by 윤희슬 on 5/6/24.
+//
+
+import Foundation
---
+++ Tving_CloneProject/Tving_CloneProject/Network/Main/MainTargetType.swift
@@ -0,0 +1,8 @@
+//
+//  MainTargetType.swift
+//  Tving_CloneProject
+//
+//  Created by 윤희슬 on 5/6/24.
+//
+
+import Foundation
