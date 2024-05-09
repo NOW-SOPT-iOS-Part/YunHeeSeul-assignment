d190acb76b4da2fb2f3f62e986d163fc7cd111b9
seuriseuljjeok <heeseul09@naver.com>
2024-05-10 03:21:33 +0900

    [Feat] 일별영화순위 뷰 UI 구현 (#5)

--- Tving_CloneProject/Tving_CloneProject/Scene/Main/Views/MovieView.swift
+++ Tving_CloneProject/Tving_CloneProject/Scene/Main/Views/MovieView.swift
@@ -11,20 +11,30 @@ import SnapKit
 import Then
 
 class MovieView: UIView {
-
+    
     // MARK: - UI Properties
     
-    private let movieView = UIView()
+    private let dailyBoxOfficeCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    private var loadingIndicator = UIActivityIndicatorView()
     
     
     // MARK: - Properties
     
+    private var dailyBoxOfficeData: [DailyBoxOfficeList] = []
+    
+    
+    // MARK: - Life Cycles
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        getDailyBoxOffice()
         setHierarchy()
         setLayout()
         setStyle()
+        setDelegate()
+        registerCell()
     }
     
     required init?(coder: NSCoder) {
@@ -36,20 +46,83 @@ class MovieView: UIView {
 private extension MovieView {
     
     func setHierarchy() {
-        self.addSubview(movieView)
+        
+        self.addSubviews(dailyBoxOfficeCollectionView, loadingIndicator)
     }
     
     func setLayout() {
-        movieView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+        
+        dailyBoxOfficeCollectionView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(Constant.Screen.topSafeAreaHeight + 80)
+            $0.horizontalEdges.bottom.equalToSuperview().inset(10)
         }
     }
     
     func setStyle() {
-        movieView.do {
-            $0.isHidden = true
-            $0.backgroundColor = UIColor(resource: .grey3)
+        
+        dailyBoxOfficeCollectionView.do {
+            $0.backgroundColor = UIColor(resource: .black)
+        }
+        
+        loadingIndicator.do {
+            $0.frame = self.bounds
+            $0.color = UIColor(resource: .white)
+            $0.backgroundColor = UIColor(resource: .black)
         }
     }
+    
+    func registerCell() {
+        
+        dailyBoxOfficeCollectionView.register(DailyBoxOfficeCell.self, forCellWithReuseIdentifier: DailyBoxOfficeCell.identifier)
+    }
+    
+    func setDelegate() {
+        
+        dailyBoxOfficeCollectionView.delegate = self
+        dailyBoxOfficeCollectionView.dataSource = self
+    }
+    
+        }
+    }
+    
+}
+
+extension MovieView: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: Constant.Screen.width / 2 - 20, height: Constant.Screen.height / 3.5)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+}
+
+extension MovieView: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return self.dailyBoxOfficeData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DailyBoxOfficeCell.identifier, for: indexPath) as? DailyBoxOfficeCell else { return UICollectionViewCell() }
+        cell.setCell(contents: dailyBoxOfficeData[indexPath.row])
+        
+        return cell
+    }
+    
+    
+    
     
 }
